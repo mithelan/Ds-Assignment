310ffc99991604b4ce8fc51037e871dca4e7aa4d
Vithurson719 <52669965+Vithurson719@users.noreply.github.com>
2021-05-31 19:06:03 +0530

    Update FireAlarmResource.java
    Updated the Changes

--- firealarm/src/main/java/com/example/firealarm/Resource/FireAlarmResource.java
+++ firealarm/src/main/java/com/example/firealarm/Resource/FireAlarmResource.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import java.util.List;
 
