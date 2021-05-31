334fa708035be9a0006e2316bf49db0a011627b8
Vithurson719 <52669965+Vithurson719@users.noreply.github.com>
2021-05-31 19:05:30 +0530

    Updated Changes
    i have added the requirements

--- firealarm/src/main/java/com/example/firealarm/Resource/FireAlarmResource.java
+++ firealarm/src/main/java/com/example/firealarm/Resource/FireAlarmResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,4 +26,14 @@ public List<Roomdetails> showall(){
    return fireAlarmRepo.findAll();
 }
 
+@PostMapping(path = "/roomDetails")
+public void addRoom(@RequestBody Roomdetails roomDetails) {
+        fireAlarmRepo.save(roomDetails);
+  }
+   
+@DeleteMapping("/roomDetails/{id}")
+  public void deleteEmployee(@PathVariable Long id) {
+    fireAlarmRepo.deleteById(id);
+  } 
+   
 }
